a98d9bbbda5dfff3acb394438af485d644e7c112
barachenadia <barache.nadia@yahoo.fr>
2013-12-07 02:52:14 +0100

    Updating_users

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutoApp::Application.config.secret_key_base = '1a37d411fa89ae0d5abce3fef49249c51794683208d69752d0fbe5db978df6ccd8a097bf89cd34b73802db6a2c9a13c0fa0ebbfec895b5894e475da62925a126'
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "User pages" do
+
+  subject { page }
+
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+  end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  	
+  end
+end
+
+
+
